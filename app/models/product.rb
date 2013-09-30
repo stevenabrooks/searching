bed8002a631297ac2ce38fbc790b78a1bd1472b5
Steven Brooks <brooks.stevena@gmail.com>
2013-09-30 10:54:10 -0400

    created scaffold for products with three attributes

---
+++ app/models/product.rb
@@ -0,0 +1,3 @@
+class Product < ActiveRecord::Base
+  attr_accessible :grade, :name, :system
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Search::Application.config.secret_token = '2c82915b03a837a4c5073947fa67ae4f2bfd3c492e2970e1f304763a86ff71c411bd1676c7fb17c4e73d4277b66fedb4fbc1d79f572b9847ff85011d8520312d'
